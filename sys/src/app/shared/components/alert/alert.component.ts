661f43a597694323de9ee136b2d10de20bfdbaff
Ara Macao <tarduvias@gmail.com>
2025-06-27 09:47:51 -0600

    [Fix] Progress bar in alert (sys)
    Se agrega barra de progreso en las alertas para indicar el tiempo que se verá dicha alerta.

--- sys/src/app/shared/components/alert/alert.component.ts
+++ sys/src/app/shared/components/alert/alert.component.ts
@@ -1,4 +1,20 @@
-import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnDestroy,
+  Output,
+  SimpleChanges
+} from '@angular/core';
+import {
+  finalize,
+  interval,
+  Subject,
+  takeUntil,
+  tap,
+  timer
+} from 'rxjs';
 
 @Component({
   selector: 'shared-alert',
@@ -6,21 +22,49 @@ import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from
   templateUrl: './alert.component.html',
   styleUrl: './alert.component.sass'
 })
-export class AlertComponent implements OnChanges {
+export class AlertComponent implements OnChanges, OnDestroy {
   @Input() message = '';
-  @Output() messageChange = new EventEmitter<string>();
+  @Output() readonly messageChange = new EventEmitter<string>();
   @Input() success = false;
+  progress = 0;
+  private readonly _stop$ = new Subject<void>();
+
+  ngOnDestroy() {
+    this._stop$.next();
+    this._stop$.complete();
+  }
+
+  private Reset() {
+    this.progress = 0;
+    this._stop$.next();
+  }
 
   ngOnChanges(
     changes: SimpleChanges
   ) {
+    this.Reset();
     if (!changes['message'] || !this.message) return;
-    setTimeout(() => {
-      this.messageChange.emit('');
-    }, 5000);
+
+    interval(100)
+      .pipe(
+        takeUntil(timer(5000)),
+        takeUntil(this._stop$),
+        tap(() => (this.progress = Math.min(this.progress + 2, 100))),
+        finalize(() => this.messageChange.emit(''))
+      )
+      .subscribe();
   }
 
   Hide() {
+    this.Reset();
     this.message = '';
+    this.messageChange.emit('');
+  }
+
+  GetClass() {
+    return {
+      'is-success': this.success,
+      'is-danger': !this.success
+    };
   }
 }
