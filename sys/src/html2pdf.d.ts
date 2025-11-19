513a6119c8b6185411af67b7145fd89a62dbd2cc
Ara Macao <tarduvias@gmail.com>
2025-11-19 13:29:51 -0600

    [Del] html2pdf (sys)

--- sys/src/html2pdf.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'html2pdf.js' {
-    // Si quieres usar el “objeto” sin tipos,
-    // basta con declarar cualquier cosa a modo de placeholder:
-    const html2pdf: any;
-    export default html2pdf;
-}
