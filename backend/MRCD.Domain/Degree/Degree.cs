fb38c5360bbe4fff6629135fb757235af5fa22d6
Josh <tarduvias@gmail.com>
2025-12-12 00:49:18 +0000

    add(dom): degree entity

---
+++ backend/MRCD.Domain/Degree/Degree.cs
@@ -0,0 +1,35 @@
+using MRCD.Domain.Common;
+
+namespace MRCD.Domain.Degree;
+
+public sealed class Degree
+{
+    private Degree() { }
+    public Guid ID { get; private set; }
+    public string Name { get; private set; } = default!;
+
+    public static Result<Degree> Create(
+        string name
+    )
+    {
+        if (string.IsNullOrWhiteSpace(name))
+            return Result<Degree>.Failure("El nombre del grado académico no puede estar vacío");
+        return Result<Degree>.Success(new()
+        {
+            ID = Guid.NewGuid(),
+            Name = name.Trim()
+        });
+    }
+
+    public Result SetName(
+        string name
+    )
+    {
+        if (string.IsNullOrWhiteSpace(name))
+            return Result.Failure("El nombre del grado académico no puede estar vacío");
+        if (name.Trim().Equals(Name))
+            return Result.Failure("El nombre ya está en uso");
+        Name = name.Trim();
+        return Result.Success();
+    }
+}
