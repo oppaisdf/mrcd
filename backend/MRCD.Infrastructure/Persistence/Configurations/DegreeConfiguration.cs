4dd04a335ada64d6fa1880e78e0309524405890a
Josh <tarduvias@gmail.com>
2025-12-15 00:08:14 +0000

    add(dom, infra): degree configuration

---
+++ backend/MRCD.Infrastructure/Persistence/Configurations/DegreeConfiguration.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MRCD.Domain.Degree;
+
+namespace MRCD.Infrastructure.Persistence.Configurations;
+
+internal sealed class DegreeConfiguration : IEntityTypeConfiguration<Degree>
+{
+    public void Configure(
+        EntityTypeBuilder<Degree> builder
+    )
+    {
+        builder.ToTable("degree");
+        builder
+            .HasKey(e => e.ID)
+            .HasName("PK_Degree_ID");
+        builder
+            .Property(e => e.ID)
+            .ValueGeneratedNever();
+        builder
+            .Property(e => e.Name)
+            .HasMaxLength(30);
+    }
+}
