6f9af4c9935169d877575c2ffaaefdbdb429c83c
Josh <tarduvias@gmail.com>
2025-12-22 17:48:10 +0000

    add(app): get base entity handler

---
+++ backend/MRCD.Application/BaseEntity/GetBaseEntity/GetBaseEntityHandler.cs
@@ -0,0 +1,21 @@
+using MRCD.Application.Abstracts.Handlers;
+using MRCD.Application.BaseEntity.Contracts;
+using MRCD.Domain.Common;
+
+namespace MRCD.Application.BaseEntity.GetBaseEntity;
+
+internal sealed class GetBaseEntityHandler<TEntity>(
+    IBaseEntityRepository<TEntity> repo
+) : IBaseQueryHandler<TEntity, IEnumerable<TEntity>> where TEntity : Domain.Common.BaseEntity
+{
+    private readonly IBaseEntityRepository<TEntity> _repo = repo;
+
+    public Task<Result<IEnumerable<TEntity>>> HandleAsync(
+        CancellationToken cancellationToken
+    ) => _repo
+        .ToListAsync(cancellationToken)
+        .ContinueWith(r =>
+            Result<IEnumerable<TEntity>>.Success(r.Result),
+            cancellationToken
+        );
+}
