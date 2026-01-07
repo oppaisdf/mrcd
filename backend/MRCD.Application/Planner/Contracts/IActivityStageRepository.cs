5dac1567c9b632998e316f9aea21699b6531fa36
Ara Macao <tarduvias@gmail.com>
2026-01-07 14:37:54 +0000

    add(app, infra): add/delete functions in activity-stage repo

--- backend/MRCD.Application/Planner/Contracts/IActivityStageRepository.cs
+++ backend/MRCD.Application/Planner/Contracts/IActivityStageRepository.cs
@@ -4,5 +4,8 @@ namespace MRCD.Application.Planner.Contracts;
 
 public interface IActivityStageRepository
 {
+    void Add(ActivityStage activityStage);
+    Task<bool> AlreadyExistsAsync(Guid activityId, Guid stageId, Guid? userId, CancellationToken cancellationToken);
+    Task DeleteAsync(Guid activityId, Guid stageId, Guid? userId, CancellationToken cancellationToken);
     Task<List<ActivityStage>> StagesByActivityToListAsync(Guid activityId, CancellationToken cancellationToken);
 }
--- backend/MRCD.Infrastructure/Repositories/ActivityStageRepository.cs
+++ backend/MRCD.Infrastructure/Repositories/ActivityStageRepository.cs
@@ -10,6 +10,38 @@ internal sealed class ActivityStageRepository(
 {
     private readonly Persistence.AppContext _app = app;
 
+    public void Add(
+        ActivityStage activityStage
+    ) => _app
+        .Add(activityStage);
+
+    public Task<bool> AlreadyExistsAsync(
+        Guid activityId,
+        Guid stageId,
+        Guid? userId,
+        CancellationToken cancellationToken
+    ) => _app
+        .ActivitiesStages
+        .AnyAsync(a =>
+            a.ActivityId == activityId
+            && a.StageId == stageId
+            && a.UserId == userId,
+            cancellationToken
+        );
+
+    public Task DeleteAsync(
+        Guid activityId,
+        Guid stageId,
+        Guid? userId,
+        CancellationToken cancellationToken
+    ) => _app
+        .ActivitiesStages
+        .Where(a =>
+            a.ActivityId == activityId
+            && a.StageId == stageId
+            && a.UserId == userId
+        ).ExecuteDeleteAsync(cancellationToken);
+
     public Task<List<ActivityStage>> StagesByActivityToListAsync(
         Guid activityId,
         CancellationToken cancellationToken
