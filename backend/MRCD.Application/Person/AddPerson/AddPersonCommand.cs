761608a12cece6eb503455771d586ead09468746
Ara Macao <tarduvias@gmail.com>
2026-01-07 16:04:01 +0000

    add(app): add person handler

---
+++ backend/MRCD.Application/Person/AddPerson/AddPersonCommand.cs
@@ -0,0 +1,20 @@
+using MRCD.Application.Abstracts.Handlers;
+
+namespace MRCD.Application.Person.AddPerson;
+
+public sealed record AddPersonCommand(
+    string Name,
+    bool IsMasculine,
+    bool IsSunday,
+    DateOnly DOB,
+    string Address,
+    string? Phone,
+    Guid DegreeId,
+    IEnumerable<AddSimpleParentCommand> Parents
+) : ICommand<Guid>;
+
+public sealed record AddSimpleParentCommand(
+    string Name,
+    bool IsMasculine,
+    string? Phone
+);
---
+++ backend/MRCD.Application/Person/AddPerson/AddPersonHandler.cs
@@ -0,0 +1,110 @@
+using MRCD.Application.Abstracts;
+using MRCD.Application.Abstracts.Handlers;
+using MRCD.Application.BaseEntity.Contracts;
+using MRCD.Application.Parent.Contracts;
+using MRCD.Application.Person.Contracts;
+using MRCD.Application.Services.CommonService;
+using MRCD.Domain.Common;
+using MRCD.Domain.Degree;
+
+namespace MRCD.Application.Person.AddPerson;
+
+internal sealed class AddPersonHandler(
+    IPersonRepository person,
+    ICommonService service,
+    IBaseEntityRepository<Degree> degree,
+    IParentRepository parent,
+    IParentPersonRepository parentPerson,
+    IPersistenceContext save
+) : ICommandHandler<AddPersonCommand, Guid>
+{
+    private readonly IPersonRepository _person = person;
+    private readonly ICommonService _service = service;
+    private readonly IBaseEntityRepository<Degree> _degree = degree;
+    private readonly IParentRepository _parent = parent;
+    private readonly IParentPersonRepository _parentPerson = parentPerson;
+    private readonly IPersistenceContext _save = save;
+
+    private async Task<Result> AddParentsAsync(
+        Guid personId,
+        IEnumerable<AddSimpleParentCommand> rawParents,
+        CancellationToken ct
+    )
+    {
+        var parents = rawParents.Distinct().ToList();
+        if (parents.Count > 2)
+            return Result.Failure("No puede agregar más de dos padres");
+        foreach (var parent in parents)
+        {
+            var normalizedName = _service.NormalizeString(parent.Name);
+            var existingParent = await _parent.GetByNameAsync(normalizedName, ct);
+            if (existingParent is not null)
+            {
+                _parentPerson.Add(new(
+                    existingParent.ID,
+                    personId,
+                    true
+                ));
+                continue;
+            }
+            var phone = string.IsNullOrWhiteSpace(parent.Phone)
+                ? null : parent.Phone.Trim();
+            if (phone is not null && _service.HasOnlyNumbers(phone))
+                return Result.Failure($"El teléfono {phone} es inválido para el padre {parent.Name}");
+            var parentResult = Domain.Parent.Parent.Create(
+                parent.Name,
+                normalizedName,
+                parent.IsMasculine,
+                phone
+            );
+            if (!parentResult.IsSuccess)
+                return Result.Failure(parentResult.Error!);
+            _parent.Add(parentResult.Value!);
+            _parentPerson.Add(new(
+                parentResult.Value!.ID,
+                personId,
+                true
+            ));
+        }
+        return Result.Success();
+    }
+
+    public async Task<Result<Guid>> HandleAsync(
+        AddPersonCommand command,
+        CancellationToken cancellationToken
+    )
+    {
+        var phone = string.IsNullOrWhiteSpace(command.Phone)
+            ? null : command.Phone.Trim();
+        if (phone is not null && !_service.HasOnlyNumbers(phone))
+            return Result<Guid>.Failure("El número telefónico no es válido");
+        var normalizedName = _service.NormalizeString(command.Name);
+        var personResult = Domain.Person.Person.Create(
+            command.Name,
+            normalizedName,
+            command.IsMasculine,
+            command.IsSunday,
+            command.DOB,
+            command.DegreeId,
+            phone,
+            command.Address
+        );
+        if (!personResult.IsSuccess)
+            return Result<Guid>.Failure(personResult.Error!);
+
+        var alreadyExists = await _person.AlreadyExistsNameAsync(normalizedName, cancellationToken);
+        if (alreadyExists)
+            return Result<Guid>.Failure("El confirmando ya se ha registrado");
+        var degreeExists = await _degree.ExistsIdAsync(command.DegreeId, cancellationToken);
+        if (!degreeExists)
+            return Result<Guid>.Failure("El grado académico no existe");
+
+        var person = personResult.Value!;
+        _person.Add(person);
+        var parentsResult = await AddParentsAsync(person.ID, command.Parents, cancellationToken);
+        if (!parentsResult.IsSuccess)
+            return Result<Guid>.Failure(parentsResult.Error!);
+        await _save.SaveChangesAsync(cancellationToken);
+        return Result<Guid>.Success(personResult.Value!.ID);
+    }
+}
