c3db4b3b1461bf4e1ad45f35b628a957fbdd4b77
Ara Macao <tarduvias@gmail.com>
2026-01-09 15:23:11 +0000

    refac(app): change functions in delete base entity handler

--- backend/MRCD.Application/BaseEntity/DelBaseEntity/DelBaseEntityHandler.cs
+++ backend/MRCD.Application/BaseEntity/DelBaseEntity/DelBaseEntityHandler.cs
@@ -1,4 +1,5 @@
 using Microsoft.Extensions.Logging;
+using MRCD.Application.Abstracts;
 using MRCD.Application.Abstracts.Handlers;
 using MRCD.Application.BaseEntity.Contracts;
 using MRCD.Domain.Common;
@@ -7,11 +8,13 @@ namespace MRCD.Application.BaseEntity.DelBaseEntity;
 
 internal sealed class DelBaseEntityHandler<TEntity>(
     IBaseEntityRepository<TEntity> repo,
+    IPersistenceContext save,
     ILogger<DelBaseEntityHandler<TEntity>> logs
 ) : IBaseCommandHandler<DelBaseEntityCommand, TEntity>
     where TEntity : Domain.Common.BaseEntity
 {
     private readonly IBaseEntityRepository<TEntity> _repo = repo;
+    private readonly IPersistenceContext _save = save;
     private readonly ILogger<DelBaseEntityHandler<TEntity>> _logs = logs;
 
     public async Task<Result> HandleAsync(
@@ -19,11 +22,12 @@ internal sealed class DelBaseEntityHandler<TEntity>(
         CancellationToken cancellationToken
     )
     {
-        var exists = await _repo.ExistsIdAsync(command.Id, cancellationToken);
-        if (!exists)
+        var record = await _repo.GetByIdAsync(command.Id, cancellationToken);
+        if (record is null)
             return Result.Failure("El registro no existe");
-        await _repo.DeleteAsync(command.Id, cancellationToken);
+        _repo.Remove(record);
         _logs.LogInformation("Record {record} has been deleted by user {user}", command.Id, command.UserId);
+        await _save.SaveChangesAsync(cancellationToken);
         return Result.Success();
     }
 }
