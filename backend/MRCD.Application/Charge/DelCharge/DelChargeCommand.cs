80811dba079f466a47d434248d33d2e758819352
Ara Macao <tarduvias@gmail.com>
2026-01-09 15:11:11 +0000

    del(app): delete charge handler

--- backend/MRCD.Application/Charge/DelCharge/DelChargeCommand.cs
+++
@@ -1,9 +0,0 @@
-using MRCD.Application.Abstracts.Handlers;
-using MRCD.Domain.Common;
-
-namespace MRCD.Application.Charge.DelCharge;
-
-public sealed record DelChargeCommand(
-    Guid UserId,
-    Guid ChargeId
-) : ICommand<Result>;
