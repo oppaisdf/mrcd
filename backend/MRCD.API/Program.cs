869e79196cc3fc35d8bb159b1cc54c4824503706
Josh <tarduvias@gmail.com>
2026-01-11 01:14:56 +0000

    add(host): db connection

--- backend/MRCD.API/Program.cs
+++ backend/MRCD.API/Program.cs
@@ -1,6 +1,24 @@
+using MRCD.Application;
+using MRCD.Application.Abstracts.Security;
+using MRCD.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
+var connection = Environment.GetEnvironmentVariable("DB_CONNECTION")
+    ?? builder.Configuration.GetConnectionString("DB_CONNECTION")
+    ?? throw new InvalidOperationException("==== No se encontró la cadena de conexión :c ====");
+var encryptionOptions = new EncryptionOptions()
+{
+    KeyBase64 = Environment.GetEnvironmentVariable("ENCRYPTION_KEY")
+        ?? builder.Configuration["ENCRYPTION_KEY"]
+        ?? throw new InvalidOperationException("==== No se encontró la llave de encriptación :c ===="),
+    Version = "v1"
+};
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddApplication();
+builder.Services.AddInfrasctructure(connection, encryptionOptions);
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
