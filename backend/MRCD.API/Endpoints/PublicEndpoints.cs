9692764709808c11cd996b2d2a99e4a15f2384a7
Ara Macao <tarduvias@gmail.com>
2026-01-14 16:04:30 +0000

    add(api): login endpoint

--- backend/MRCD.API/Endpoints/PublicEndpoints.cs
+++ backend/MRCD.API/Endpoints/PublicEndpoints.cs
@@ -1,3 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+using MRCD.API.DTOs;
+using MRCD.API.Services;
+using MRCD.Application.Abstracts.Handlers;
+using MRCD.Application.User.DTOs;
+using MRCD.Application.User.Login;
+
 namespace MRCD.API.Endpoints;
 
 internal static class PublicEndpoints
@@ -23,5 +30,30 @@ internal static class PublicEndpoints
         .WithDescription("Verifica si la API responde correctamente")
         .Produces(StatusCodes.Status200OK)
         .WithOpenApi();
+
+        app.MapPost("/auth/login", async (
+            [FromBody] UserLoginCommand request,
+            [FromServices] ICommandHandler<UserLoginCommand, LoginDTO> handler,
+            ITokenService jwt,
+            CancellationToken ct
+        ) =>
+        {
+            var result = await handler.HandleAsync(request, ct);
+            if (!result.IsSuccess)
+                return Results.Problem(
+                    title: "Bad Request",
+                    detail: result.Error,
+                    statusCode: StatusCodes.Status400BadRequest
+                );
+            var token = jwt.Create(
+                result.Value!.UserId,
+                result.Value.Roles,
+                result.Value.Permissions
+            );
+            return Results.Ok(new TokenDTO(
+                token.AccessToken,
+                token.ExpiresAtUTC
+            ));
+        });
     }
 }
