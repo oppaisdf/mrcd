3d598d66d43dfe07bbe1414ebdfb5100d9ae2cc6
Ara Macao <tarduvias@gmail.com>
2026-01-22 14:14:51 +0000

    refac(core): move root page to folder

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { RootPage } from './app/core/layout/root.page';
+import { RootPage } from './app/core/layout/root.page/root.page';
 
 bootstrapApplication(RootPage, appConfig)
   .catch((err) => console.error(err));
