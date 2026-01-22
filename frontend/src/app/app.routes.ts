15c488aae21ab61994d7549660ea54c1fb0ebd5e
Ara Macao <tarduvias@gmail.com>
2026-01-22 16:50:13 +0000

    add(front): general router and styles

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ShellComponent } from './core/layout/shell.component/shell.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: ShellComponent
+    },
+    { path: '**', redirectTo: '' }
+];
