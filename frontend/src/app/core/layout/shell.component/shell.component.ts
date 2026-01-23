26485c60b397e031ecb8a82b2c8b2e4c3dcebddd
Ara Macao <tarduvias@gmail.com>
2026-01-23 15:30:36 +0000

    feat(core): show nav when user is authenticated

--- frontend/src/app/core/layout/shell.component/shell.component.ts
+++ frontend/src/app/core/layout/shell.component/shell.component.ts
@@ -1,6 +1,7 @@
 import { Component, DOCUMENT, inject } from '@angular/core';
 import { RouterOutlet } from "@angular/router";
 import { NavComponent } from "../nav.component/nav.component";
+import { SessionStore } from '../../stores/session.store';
 
 @Component({
   selector: 'app-shell',
@@ -11,6 +12,8 @@ import { NavComponent } from "../nav.component/nav.component";
 export class ShellComponent {
   private readonly _doc = inject(DOCUMENT);
   protected year = new Date().getFullYear();
+  private readonly _session = inject(SessionStore);
+  readonly isAuth = this._session.isAuthenticated;
 
   toggleTheme() {
     const root = this._doc.documentElement;
