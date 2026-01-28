252d63148f80446b5c1714fd4996231ee9497bbe
Ara Macao <tarduvias@gmail.com>
2026-01-28 15:29:35 +0000

    add(permission): list page

---
+++ frontend/src/app/features/permissions/list/permissions-list.page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-permissions-list.page',
+  imports: [],
+  templateUrl: './permissions-list.page.html',
+  styleUrl: './permissions-list.page.scss',
+})
+export class PermissionsListPage {
+
+}
