70c9107e88f38d870281575872a087cfc1dc5b3b
Ara Macao <tarduvias@gmail.com>
2026-01-28 15:29:21 +0000

    add(permission): create page

---
+++ frontend/src/app/features/permissions/create/permissions-create.page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-permissions-create.page',
+  imports: [],
+  templateUrl: './permissions-create.page.html',
+  styleUrl: './permissions-create.page.scss',
+})
+export class PermissionsCreatePage {
+
+}
