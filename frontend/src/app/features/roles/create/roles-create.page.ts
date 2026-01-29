8730144f48efbc203c4d0e11a27ee7abfb878afd
Ara Macao <tarduvias@gmail.com>
2026-01-29 13:49:05 +0000

    add(roles): create role page

---
+++ frontend/src/app/features/roles/create/roles-create.page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-roles-create.page',
+  imports: [],
+  templateUrl: './roles-create.page.html',
+  styleUrl: './roles-create.page.scss',
+})
+export class RolesCreatePage {
+
+}
