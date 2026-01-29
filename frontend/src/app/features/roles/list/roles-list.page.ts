c91dc6dd96284bdaa0f4236be086f58782421630
Ara Macao <tarduvias@gmail.com>
2026-01-29 13:48:45 +0000

    add(roles): list roles page

---
+++ frontend/src/app/features/roles/list/roles-list.page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-roles-list.page',
+  imports: [],
+  templateUrl: './roles-list.page.html',
+  styleUrl: './roles-list.page.scss',
+})
+export class RolesListPage {
+
+}
