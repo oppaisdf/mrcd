7fc89b1b688c2884a1681412d5e4cd33dd458611
Ara Macao <tarduvias@gmail.com>
2026-02-02 16:07:50 +0000

    fix(api): logout in error 401

--- frontend/src/app/core/api/api.interceptor.ts
+++ frontend/src/app/core/api/api.interceptor.ts
@@ -1,16 +1,18 @@
 import { HttpErrorResponse, HttpInterceptorFn } from "@angular/common/http";
 import { inject } from "@angular/core";
-import { Router } from "@angular/router";
 import { catchError, throwError } from "rxjs";
+import { AuthService } from "../auth/auth.service";
 
 export const apiErrorInterceptor: HttpInterceptorFn = (req, next) => {
-    const router = inject(Router);
+    const auth = inject(AuthService);
 
     return next(req).pipe(
         catchError((err: HttpErrorResponse) => {
             switch (err.status) {
                 case 403: break;
-                case 401: break;
+                case 401:
+                    auth.logout();
+                    break;
             }
             return throwError(() => err);
         })
