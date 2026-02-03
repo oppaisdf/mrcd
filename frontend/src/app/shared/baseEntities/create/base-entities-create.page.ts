0fcdf5963aea9f83e089ba851762a997866d1e9c
Ara Macao <tarduvias@gmail.com>
2026-02-03 19:29:15 +0000

    feat(shared): base entity create page

---
+++ frontend/src/app/shared/baseEntities/create/base-entities-create.page.ts
@@ -0,0 +1,59 @@
+import { Component, inject, input } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AlertService } from '../../alerts/services/alert.service';
+import { BaseEntitiesService } from '../services/base-entities.service';
+import { BaseEntityCommand } from '../dtos/BaseEntityCommand';
+import { UiInputComponent } from "../../../core/ui/input/ui-input.component";
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'shared-be-create',
+  imports: [ReactiveFormsModule, UiInputComponent],
+  templateUrl: './base-entities-create.page.html',
+  styleUrl: './base-entities-create.page.scss',
+})
+export class BaseEntitiesCreatePage {
+  private readonly _form = inject(FormBuilder);
+  private readonly _alert = inject(AlertService);
+  private readonly _service = inject(BaseEntitiesService);
+  private readonly _router = inject(Router);
+  private readonly _me = inject(ActivatedRoute);
+  readonly form = this._form.nonNullable.group({
+    name: ['', Validators.required]
+  });
+
+  maxLength = input.required<number>();
+  title = input.required<string>();
+  endpoint = input.required<string>();
+
+  hint() {
+    const control = this.form.controls.name;
+    if (!control.touched) return undefined;
+    if (control.valid) return undefined;
+    if (control.value?.length ?? 0 > this.maxLength())
+      return `La longitud no puede exceder los ${this.maxLength()} caracteres`;
+    return 'Este campo es requerido';
+  }
+
+  async createAsync() {
+    if (this._alert.loading()) return;
+    this.form.markAllAsTouched();
+    if (this.form.invalid) return;
+
+    this._alert.startLoading();
+    this.form.disable();
+
+    const request: BaseEntityCommand = this.form.getRawValue();
+    const response = await this._service.createAsync(this.endpoint(), request);
+
+    this._alert.clear();
+    this.form.enable();
+
+    if (!response.isSuccess) {
+      this._alert.error(response.message!);
+      return;
+    }
+    this._alert.success("Registro creado con éxito");
+    this._router.navigate(['..'], { relativeTo: this._me });
+  }
+}
