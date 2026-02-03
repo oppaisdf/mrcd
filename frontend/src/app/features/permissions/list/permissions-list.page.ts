e640946e72bef8d5d9035ed898084926ab44badc
Ara Macao <tarduvias@gmail.com>
2026-02-03 20:31:44 +0000

    refac(permission): use base entity list in permission list page

--- frontend/src/app/features/permissions/list/permissions-list.page.ts
+++ frontend/src/app/features/permissions/list/permissions-list.page.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
+import { BaseEntitiesListPage } from "../../../shared/baseEntities/list/base-entities-list.page";
 
 @Component({
   selector: 'app-permissions-list.page',
-  imports: [],
+  imports: [RouterLink, BaseEntitiesListPage],
   templateUrl: './permissions-list.page.html',
   styleUrl: './permissions-list.page.scss',
 })
