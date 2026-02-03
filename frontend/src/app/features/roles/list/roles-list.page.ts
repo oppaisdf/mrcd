b28c0c377073413d333a2b928e950eebc0845a04
Ara Macao <tarduvias@gmail.com>
2026-02-03 20:20:43 +0000

    refac(roles): use base entity list in roles page

--- frontend/src/app/features/roles/list/roles-list.page.ts
+++ frontend/src/app/features/roles/list/roles-list.page.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { BaseEntitiesListPage } from "../../../shared/baseEntities/list/base-entities-list.page";
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-roles-list.page',
-  imports: [],
+  imports: [BaseEntitiesListPage, RouterLink],
   templateUrl: './roles-list.page.html',
   styleUrl: './roles-list.page.scss',
 })
 export class RolesListPage {
-
 }
