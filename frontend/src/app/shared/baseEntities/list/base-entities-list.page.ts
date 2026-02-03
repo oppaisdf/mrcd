e3a1866e692ce95489f4108c12455cd40a9bb5f0
Ara Macao <tarduvias@gmail.com>
2026-02-03 20:20:22 +0000

    add(shared): list base entity page

---
+++ frontend/src/app/shared/baseEntities/list/base-entities-list.page.ts
@@ -0,0 +1,41 @@
+import { Component, effect, inject, input, signal } from '@angular/core';
+import { BaseEntitiesService } from '../services/base-entities.service';
+import { AlertService } from '../../alerts/services/alert.service';
+import { BaseEntityDTO } from '../dtos/BaseEntityDTO';
+
+@Component({
+  selector: 'shared-be-list',
+  imports: [],
+  templateUrl: './base-entities-list.page.html',
+  styleUrl: './base-entities-list.page.scss',
+})
+export class BaseEntitiesListPage {
+  private readonly _service = inject(BaseEntitiesService);
+  private readonly _alert = inject(AlertService);
+  title = input.required<string>();
+  endpoint = input.required<string>();
+  showActions = input<boolean>(true);
+  readonly items = signal<Array<BaseEntityDTO>>([]);
+
+  private _ = effect(() => this.loadAsync());
+  private async loadAsync() {
+    const response = await this._service.toListAsync(this.endpoint());
+    if (!response.isSuccess)
+      this._alert.error(response.message!);
+    else this.items.set(response.data ?? []);
+  }
+
+  protected async deleteAsync(
+    id: string
+  ) {
+    if (this._alert.loading()) return;
+    this._alert.startLoading();
+
+    const response = await this._service.deleteAsync(this.endpoint(), id);
+
+    this._alert.clear();
+    if (response.isSuccess)
+      this._alert.success("Se ha eliminado el registro correctamente");
+    else this._alert.error(response.message!);
+  }
+}
