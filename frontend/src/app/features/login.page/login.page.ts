a5d51f925e88658a53abf3e2988076c9c7cb9d7e
Ara Macao <tarduvias@gmail.com>
2026-02-03 15:33:31 +0000

    refac(login): use alert service in login page

--- frontend/src/app/features/login.page/login.page.ts
+++ frontend/src/app/features/login.page/login.page.ts
@@ -3,6 +3,7 @@ import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService } from '../../core/auth/auth.service';
 import { Router } from '@angular/router';
 import { UiInputComponent } from "../../core/ui/input/ui-input.component";
+import { AlertService } from '../../shared/alerts/services/alert.service';
 
 @Component({
   selector: 'app-login.page',
@@ -14,26 +15,28 @@ export class LoginPage {
   private readonly _form = inject(FormBuilder);
   private readonly _service = inject(AuthService);
   private readonly _router = inject(Router);
+  private readonly _alert = inject(AlertService);
   readonly form = this._form.nonNullable.group({
     username: ['', Validators.required],
     password: ['', Validators.required]
   });
 
   readonly isLoading = signal<boolean>(false);
-  readonly message = signal<string>('');
 
   async loginAsync() {
     this.form.markAllAsTouched();
     if (this.form.invalid) return;
     if (this.isLoading()) return;
 
+    this._alert.startLoading();
     this.isLoading.set(true);
     this.form.disable();
     const request = this.form.getRawValue();
     const response = await this._service.loginAsync(request.username, request.password);
     this.isLoading.set(false);
     this.form.enable();
-    if (response) this.message.set(response);
+    this._alert.clear();
+    if (response) this._alert.error(response);
     else this._router.navigateByUrl('/');
   }
 
