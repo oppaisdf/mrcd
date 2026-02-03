ca9c2b15dd6b087a1f427e704af78833d77b0c7f
Ara Macao <tarduvias@gmail.com>
2026-02-03 15:33:09 +0000

    refac(core): use alerts in root page

--- frontend/src/app/core/layout/root.page/root.page.ts
+++ frontend/src/app/core/layout/root.page/root.page.ts
@@ -1,13 +1,17 @@
 import { Component, inject } from '@angular/core';
 import { RouterOutlet } from "@angular/router";
 import { NavDirectionService } from '../../ui/transitions/nav-direction.service';
+import { AlertComponent } from "../../../shared/alerts/alert.component/alert.component";
+import { AlertService } from '../../../shared/alerts/services/alert.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AlertComponent],
   templateUrl: './root.page.html',
   styleUrl: './root.page.scss',
 })
 export class RootPage {
   private readonly _navDir = inject(NavDirectionService); // Registro para listener
+  private readonly _alert = inject(AlertService);
+  readonly alertIsOpen = this._alert.isOpen;
 }
