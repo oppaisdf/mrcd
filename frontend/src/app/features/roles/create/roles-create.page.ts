c3f8ab931ea728143ba481c8d6ba5411f1b59a59
Ara Macao <tarduvias@gmail.com>
2026-02-03 19:30:17 +0000

    add(roles): use base entity component in roles page

--- frontend/src/app/features/roles/create/roles-create.page.ts
+++ frontend/src/app/features/roles/create/roles-create.page.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { BaseEntitiesCreatePage } from "../../../shared/baseEntities/create/base-entities-create.page";
 
 @Component({
   selector: 'app-roles-create.page',
-  imports: [],
+  imports: [BaseEntitiesCreatePage],
   templateUrl: './roles-create.page.html',
   styleUrl: './roles-create.page.scss',
 })
