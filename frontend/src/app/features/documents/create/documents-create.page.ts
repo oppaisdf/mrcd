2d53dd30010517f0ef05eea5d051c72cbf657386
Ara Macao <tarduvias@gmail.com>
2026-02-04 13:48:21 +0000

    add(documents): create page

---
+++ frontend/src/app/features/documents/create/documents-create.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { BaseEntitiesCreatePage } from "../../../shared/baseEntities/create/base-entities-create.page";
+
+@Component({
+  selector: 'app-documents-create.page',
+  imports: [BaseEntitiesCreatePage],
+  templateUrl: './documents-create.page.html',
+  styleUrl: './documents-create.page.scss',
+})
+export class DocumentsCreatePage {
+
+}
