be71a34f74c0a4c79a7ba8cc239f3652365c8866
Ara Macao <tarduvias@gmail.com>
2026-02-04 14:12:20 +0000

    add(sacraments): create page

---
+++ frontend/src/app/features/sacraments/create/sacraments-create.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { BaseEntitiesCreatePage } from "../../../shared/baseEntities/create/base-entities-create.page";
+
+@Component({
+  selector: 'app-sacraments-create.page',
+  imports: [BaseEntitiesCreatePage],
+  templateUrl: './sacraments-create.page.html',
+  styleUrl: './sacraments-create.page.scss',
+})
+export class SacramentsCreatePage {
+
+}
