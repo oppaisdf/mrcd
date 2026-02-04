ad7077c75c70b4154a4a762913daf30914b3b0b3
Ara Macao <tarduvias@gmail.com>
2026-02-04 14:01:40 +0000

    add(degrees): create page

---
+++ frontend/src/app/features/degrees/create/degrees-create.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { BaseEntitiesCreatePage } from "../../../shared/baseEntities/create/base-entities-create.page";
+
+@Component({
+  selector: 'app-degrees-create.page',
+  imports: [BaseEntitiesCreatePage],
+  templateUrl: './degrees-create.page.html',
+  styleUrl: './degrees-create.page.scss',
+})
+export class DegreesCreatePage {
+
+}
