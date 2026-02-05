ed485c7d359fc5498b37d6c65914c962b64a6cc2
Ara Macao <tarduvias@gmail.com>
2026-02-05 21:06:15 +0000

    add(user): user form component

---
+++ frontend/src/app/features/users/form/user-form.component.ts
@@ -0,0 +1,65 @@
+import { Component, effect, inject, input, output } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { UiInputComponent } from '../../../core/ui/input/ui-input.component';
+import { UiSelectComponent } from "../../../core/ui/select/ui-select.component";
+import { SelectItem } from '../../../core/ui/select/SelectItem';
+import { UserVM } from '../vms/UserVM';
+
+@Component({
+  selector: 'user-form',
+  imports: [
+    ReactiveFormsModule,
+    UiInputComponent,
+    UiSelectComponent
+  ],
+  templateUrl: './user-form.component.html',
+  styleUrl: './user-form.component.scss',
+})
+export class UserFormComponent {
+  private readonly _form = inject(FormBuilder);
+  readonly form = this._form.nonNullable.group({
+    username: ['', [Validators.required, Validators.maxLength(10)]],
+    password: ['', [
+      Validators.required,
+      Validators.pattern('^(?=.*[A-Z])(?=.*\\d)(?=.*[\\W_]).{6,}$')
+    ]],
+    isActive: [true, Validators.required]
+  });
+
+  mode = input.required<'Crear' | 'Editar'>();
+  user = input.required<UserVM>();
+  submit = output<UserVM>();
+  readonly states: Array<SelectItem<boolean>> = [
+    {
+      label: 'Activo',
+      value: true
+    }, {
+      label: 'Inactivo',
+      value: false
+    }
+  ];
+
+  constructor() {
+    effect(() => {
+      const user = this.user();
+      this.form.patchValue({
+        username: user.username,
+        password: user.password,
+        isActive: user.isActive
+      }, { emitEvent: false });
+      if (user.isActive) {
+        this.form.enable();
+        return;
+      }
+      this.form.disable();
+      this.form.controls.isActive.enable();
+    });
+  }
+
+  sendSubmit() {
+    this.form.markAsTouched();
+    if (this.form.invalid) return;
+    const user: UserVM = this.form.getRawValue();
+    this.submit.emit(user);
+  }
+}
---
+++ frontend/src/app/features/users/vms/UserVM.ts
@@ -0,0 +1,5 @@
+export type UserVM = {
+    username: string;
+    password: string;
+    isActive: boolean;
+};
