216cd6eb6febabbfb3956e9dd92aacbead609dd3
Ara Macao <tarduvias@gmail.com>
2026-02-05 10:08:02 -0600

    fix(core): redirect to forbidden in error response

--- frontend/src/app/core/api/api.interceptor.ts
+++ frontend/src/app/core/api/api.interceptor.ts
@@ -2,14 +2,18 @@ import { HttpErrorResponse, HttpInterceptorFn } from "@angular/common/http";
 import { inject } from "@angular/core";
 import { catchError, throwError } from "rxjs";
 import { AuthService } from "../auth/auth.service";
+import { Router } from "@angular/router";
 
 export const apiErrorInterceptor: HttpInterceptorFn = (req, next) => {
     const auth = inject(AuthService);
+    const router = inject(Router);
 
     return next(req).pipe(
         catchError((err: HttpErrorResponse) => {
             switch (err.status) {
-                case 403: break;
+                case 403:
+                    router.navigateByUrl('/forbidden');
+                    break;
                 case 401:
                     auth.logout();
                     break;
