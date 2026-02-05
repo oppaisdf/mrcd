301cfdd1464148feacdad44fab830a44e74ed023
Ara Macao <tarduvias@gmail.com>
2026-02-05 20:01:27 +0000

    feat(core): close alert in error codes 403 and 401

--- frontend/src/app/core/api/api.interceptor.ts
+++ frontend/src/app/core/api/api.interceptor.ts
@@ -3,19 +3,23 @@ import { inject } from "@angular/core";
 import { catchError, throwError } from "rxjs";
 import { AuthService } from "../auth/auth.service";
 import { Router } from "@angular/router";
+import { AlertService } from "../../shared/alerts/services/alert.service";
 
 export const apiErrorInterceptor: HttpInterceptorFn = (req, next) => {
     const auth = inject(AuthService);
     const router = inject(Router);
+    const alert = inject(AlertService);
 
     return next(req).pipe(
         catchError((err: HttpErrorResponse) => {
             switch (err.status) {
                 case 403:
                     router.navigateByUrl('/forbidden');
+                    alert.clear();
                     break;
                 case 401:
                     auth.logout();
+                    alert.clear();
                     break;
             }
             return throwError(() => err);
