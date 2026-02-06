55a18c7ecf869803f0dfe60317f566b044f7a78c
Ara Macao <tarduvias@gmail.com>
2026-02-06 14:14:11 +0000

    del(users): user dto

--- frontend/src/app/features/users/dtos/UserDTO.ts
+++
@@ -1,8 +0,0 @@
-import { UsedRoleResponse } from "../../roles/responses/UsedRole.response";
-
-export type UserDTO = {
-    id: string;
-    username: string;
-    isActive: boolean;
-    roles: Array<UsedRoleResponse>;
-};
