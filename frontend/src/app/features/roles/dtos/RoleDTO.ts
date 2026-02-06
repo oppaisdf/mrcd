fcf4245246bfe86f2e1e3c2c997062f6e4d961b6
Ara Macao <tarduvias@gmail.com>
2026-02-06 14:03:32 +0000

    del(roles): role dto

--- frontend/src/app/features/roles/dtos/RoleDTO.ts
+++
@@ -1,7 +0,0 @@
-import { PermissionResponse } from "../../permissions/reponses/Permission.response";
-
-export type RoleDTO = {
-    roleID: string;
-    roleName: string;
-    permissions: Array<PermissionResponse>;
-};
