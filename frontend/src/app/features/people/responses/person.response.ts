5e0ca7d3583f7a2df2b9087a7c6033b420820951
Ara Macao <tarduvias@gmail.com>
2026-02-09 17:36:12 +0000

    add(people): person responses

---
+++ frontend/src/app/features/people/responses/person.response.ts
@@ -0,0 +1,11 @@
+export type PersonResponse = {
+    name: string;
+    isActive: boolean;
+    isMasculine: boolean;
+    isSunday: boolean;
+    dob: Date;
+    degreeId: string;
+    parish?: string;
+    adress?: string;
+    phone?: string;
+};
---
+++ frontend/src/app/features/people/vms/person.vm.ts
@@ -0,0 +1,11 @@
+export type PersonVM = {
+    name: string | null;
+    isActive: boolean | null;
+    isMasculine: boolean | null;
+    isSunday: boolean | null;
+    parish: string | null;
+    address: string | null;
+    phone: string | null;
+    degreeId: string | null;
+    dob: Date | null;
+};
