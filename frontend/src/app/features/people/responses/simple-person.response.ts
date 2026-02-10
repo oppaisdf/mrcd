6e559f0f2f8bf24995fe464ed050626bb16ed59d
Ara Macao <tarduvias@gmail.com>
2026-02-10 15:45:13 +0000

    add(people):  list people function in service

---
+++ frontend/src/app/features/people/responses/simple-person.response.ts
@@ -0,0 +1,4 @@
+export type SimplePersonResponse = {
+    id: string;
+    name: string;
+};
--- frontend/src/app/features/people/services/person.service.ts
+++ frontend/src/app/features/people/services/person.service.ts
@@ -1,6 +1,8 @@
 import { inject, Injectable } from '@angular/core';
 import { ApiService } from '../../../core/api/api.service';
 import { CreatePersonRequest } from '../requests/create-person.request';
+import { PagedResult } from '../../../core/api/api.types';
+import { SimplePersonResponse } from '../responses/simple-person.response';
 
 @Injectable()
 export class PersonService {
@@ -10,5 +12,20 @@ export class PersonService {
     request: CreatePersonRequest
   ) {
     return this._api.postAsync<CreatePersonRequest, string>('/person', request);
+  }
+
+  public toListAsync(
+    isActive: boolean,
+    page: number,
+    name?: string,
+    isMasculine?: boolean
+  ) {
+    const params: Record<string, any> = {
+      page: page,
+      isActive: isActive,
+      name: name,
+      isMasculine: isMasculine
+    };
+    return this._api.getAsync<PagedResult<SimplePersonResponse>>('/person', params);
   }
 }
