b006fabe3a2b1b4a213666e3cf4989a49f538c26
Ara Macao <tarduvias@gmail.com>
2026-02-10 13:48:24 +0000

    refac(people): use parent component in create person page

--- frontend/src/app/features/people/create/person-create.page.ts
+++ frontend/src/app/features/people/create/person-create.page.ts
@@ -2,21 +2,17 @@ import { Component, inject, signal } from '@angular/core';
 import { PersonFormComponent } from "../form/person-form.component";
 import { PersonVM } from '../vms/person.vm';
 import { CreatePersonRequest } from '../requests/create-person.request';
-import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 import { CreateSimpleParentRequest } from '../../parents/requests/create-simple-parent.request';
-import { UiInputComponent } from "../../../core/ui/input/ui-input.component";
-import { SelectItem } from '../../../core/ui/select/SelectItem';
-import { UiSelectComponent } from "../../../core/ui/select/ui-select.component";
 import { AlertService } from '../../../shared/alerts/services/alert.service';
 import { PersonService } from '../services/person.service';
+import { PersonParentComponent } from "../parents/person-parent.component";
 
 @Component({
   selector: 'person-create.page',
   imports: [
     PersonFormComponent,
-    ReactiveFormsModule,
-    UiInputComponent,
-    UiSelectComponent
+    PersonParentComponent
   ],
   templateUrl: './person-create.page.html',
   styleUrl: './person-create.page.scss',
@@ -31,17 +27,9 @@ export class PersonCreatePage {
     phone: ['']
   });
 
-  readonly parents = signal<Array<CreateSimpleParentRequest>>([]);
-  get fullParents() { return this.parents().length === 2; }
-  readonly genders: Array<SelectItem<boolean>> = [
-    {
-      label: 'Masculino',
-      value: true
-    }, {
-      label: 'Femenino',
-      value: false
-    }
-  ];
+  private readonly _parents = signal<Array<CreateSimpleParentRequest>>([]);
+  get parents() { return this._parents(); }
+  set parents(value) { this._parents.set(value); }
 
   async createAsync(
     person: PersonVM
@@ -57,50 +45,12 @@ export class PersonCreatePage {
       address: person.address ?? '',
       phone: person.phone ?? '',
       degreeId: person.degreeId ?? '',
-      parents: this.parents()
+      parents: this._parents()
     };
     const response = await this._service.addAsync(request);
     this._alert.clear();
     if (!response.isSuccess)
       this._alert.error(response.message!);
     else this._alert.success("Se ha inscrito el confirmando correctamente");
-  }
-
-  addParent() {
-    this.form.markAllAsTouched();
-    if (this.form.invalid) return;
-    const parents = this.parents();
-    const form = this.form.getRawValue();
-    const parent: CreateSimpleParentRequest = {
-      name: form.name,
-      isMasculine: form.isMasculine,
-      phone: form.phone
-    };
-    parents.push(parent);
-    this.parents.set(parents);
-    this.form.reset();
-  }
-
-  delParent(
-    parent: CreateSimpleParentRequest
-  ) {
-    const parents = this.parents();
-    const index = parents.indexOf(parent);
-    parents.splice(index, 1);
-    this.parents.set(parents);
-  }
-
-  hint(
-    controlName: keyof typeof this.form.controls
-  ) {
-    const control = this.form.get(controlName);
-    if (!control) return `${controlName} not found`;
-    if (!control.touched || control.valid) return null;
-    const error = ({
-      'name': 'El nombre del padre es requerido y no puede superar los 80 caracteres',
-      'isMasculine': null,
-      'phone': null
-    } as const)[controlName] ?? null;
-    return error;
   }
 }
