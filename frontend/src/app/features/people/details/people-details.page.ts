b308deeb3ac33cefb978701bb12770f514c917b0
Ara Macao <tarduvias@gmail.com>
2026-02-10 17:21:41 +0000

    add(people): detals page

---
+++ frontend/src/app/features/people/details/people-details.page.ts
@@ -0,0 +1,91 @@
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PersonService } from '../services/person.service';
+import { AlertService } from '../../../shared/alerts/services/alert.service';
+import { PersonFormComponent } from "../form/person-form.component";
+import { PersonResponse } from '../responses/person.response';
+import { PersonVM } from '../vms/person.vm';
+import { UpdatePersonRequest } from '../requests/update-person.request';
+
+@Component({
+  selector: 'app-people-details.page',
+  imports: [PersonFormComponent],
+  templateUrl: './people-details.page.html',
+  styleUrl: './people-details.page.scss',
+})
+export class PeopleDetailsPage implements OnInit {
+  private readonly _me = inject(ActivatedRoute);
+  private readonly _service = inject(PersonService);
+  private readonly _alert = inject(AlertService);
+  private readonly _id: string;
+  readonly person = signal<PersonResponse>({
+    name: '',
+    isActive: false,
+    isMasculine: false,
+    isSunday: false,
+    dob: new Date(),
+    degreeId: ''
+  });
+
+  constructor() {
+    this._id = this._me.snapshot.paramMap.get("id")!;
+  }
+
+  async ngOnInit() {
+    this._alert.startLoading();
+    const response = await this._service.getByIdAsync(this._id);
+    this._alert.clear();
+    if (!response.isSuccess) {
+      if (response.message)
+        this._alert.error(response.message);
+      return;
+    }
+    if (!response.data) return;
+    const person: PersonResponse = {
+      name: response.data.name,
+      isActive: response.data.isActive,
+      isMasculine: response.data.isMasculine,
+      isSunday: response.data.isSunday,
+      dob: response.data.dob,
+      degreeId: response.data.degreeId,
+      address: response.data.address,
+      phone: response.data.phone,
+      parish: response.data.parish
+    };
+    this.person.set(person);
+  }
+
+  async updateAsync(
+    person: PersonVM
+  ) {
+    if (this._alert.loading()) return;
+    this._alert.startLoading();
+
+    const current = this.person();
+    const request: UpdatePersonRequest = {
+      name: person.name !== null && person.name !== current.name ? person.name : undefined,
+      dob: person.dob !== null && person.dob !== current.dob ? person.dob : undefined,
+      isActive: person.isActive !== null && person.isActive !== current.isActive ? person.isActive : undefined,
+      isSunday: person.isSunday !== null && person.isSunday !== current.isSunday ? person.isSunday : undefined,
+      parish: person.parish !== null && person.parish !== current.parish ? person.parish : undefined,
+      address: person.address !== null && person.address !== current.address ? person.address : undefined,
+      phone: person.phone !== null && person.phone !== current.phone ? person.phone : undefined
+    };
+    const response = await this._service.updateAsync(this._id, request);
+    if (!response.isSuccess) {
+      if (response.message)
+        this._alert.error(response.message);
+      return;
+    }
+    this._alert.success("Se ha actualizado al confirmando correctamente");
+    const newPerson = this.person();
+    if (request.name !== undefined) newPerson.name = request.name;
+    if (request.isActive !== undefined) newPerson.isActive = request.isActive;
+    if (request.isSunday !== undefined) newPerson.isSunday = request.isSunday;
+    if (request.dob !== undefined) newPerson.dob = request.dob;
+    if (request.address !== undefined) newPerson.address = request.address;
+    if (request.phone !== undefined) newPerson.phone = request.phone;
+    if (request.parish !== undefined) newPerson.parish = request.parish;
+    this.person.set(newPerson);
+  }
+}
