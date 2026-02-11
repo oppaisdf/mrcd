4cd3e60cb0f84eafaf6798d493c16bc128e251c2
Ara Macao <tarduvias@gmail.com>
2026-02-11 15:31:31 +0000

    refac(app): assign parent when exists in add parent handler

--- backend/MRCD.Application/Parent/AddParent/AddParentHandler.cs
+++ backend/MRCD.Application/Parent/AddParent/AddParentHandler.cs
@@ -24,45 +24,82 @@ internal sealed class AddParentHandler(
     private readonly IPersistenceContext _save = save;
     private readonly ILogger<AddParentHandler> _logs = logs;
 
+    private async Task<Result> AssignAsync(
+        Guid parentId,
+        Guid? personId,
+        bool isParent,
+        CancellationToken ct
+    )
+    {
+        if (personId is null) return Result.Success();
+        var existsActive = await _person.ExistsActiveAsync(personId.Value, ct);
+        if (!existsActive)
+            return Result.Failure("El confirmando/ahijado no existe o está inactivo");
+        var exists = await _parentPerson.GetAsync(personId.Value, parentId, isParent, ct);
+        if (exists is not null)
+            return Result.Failure("El padre/padrino ya se ha asignado al confirmando");
+        _parentPerson.Add(new(
+            parentId,
+            personId.Value,
+            isParent
+        ));
+        _logs.LogInformation("Parent {parent} has been assigned to person {person}", parentId, personId.Value);
+        return Result.Success();
+    }
+
+    private async Task<Result<Guid>> CreateAsync(
+        AddParentCommand command,
+        string normalizedName,
+        CancellationToken ct
+    )
+    {
+        if (
+            !string.IsNullOrWhiteSpace(command.Phone)
+            && !_service.HasOnlyNumbers(command.Phone)
+        ) return Result<Guid>.Failure("El número telefónico no es válido");
+
+        var result = Domain.Parent.Parent.Create(
+            command.ParentName,
+            normalizedName,
+            command.IsMasculine,
+            command.Phone
+        );
+        if (!result.IsSuccess)
+            return Result<Guid>.Failure(result.Error!);
+        _repo.Add(result.Value!);
+
+        var resultAssign = await AssignAsync(result.Value!.ID, command.PersonId, command.IsParent, ct);
+        if (!resultAssign.IsSuccess)
+            return Result<Guid>.Failure(resultAssign.Error!);
+
+        _logs.LogInformation("Parent {parent} has been added by user {user}", result.Value!.ID, command.UserId);
+        return Result<Guid>.Success(result.Value!.ID);
+    }
+
     public async Task<Result<Guid>> HandleAsync(
         AddParentCommand command,
         CancellationToken cancellationToken
     )
     {
-        if (
-            !string.IsNullOrWhiteSpace(command.Phone)
-            && !_service.HasOnlyNumbers(command.Phone.Trim())
-        )
-            return Result<Guid>.Failure("El número telefónico no es válido");
-        var parentResult = Domain.Parent.Parent.Create(
-            command.ParentName,
-            _service.NormalizeString(command.ParentName),
-            command.IsMasculine,
-            command.Phone
-        );
-        if (!parentResult.IsSuccess)
-            return Result<Guid>.Failure(parentResult.Error!);
-        var parent = parentResult.Value!;
-        _repo.Add(parent);
+        var normalizedName = _service.NormalizeString(command.ParentName);
+        var currentParent = await _repo.GetByNameAsync(normalizedName, cancellationToken);
 
-        var alreadyExists = await _repo.AlreadyExists(parent.NormalizedName, cancellationToken);
-        if (alreadyExists)
-            return Result<Guid>.Failure("El padre/padrino ya se ha registrado");
-
-        if (command.PersonId is not null)
+        Guid id;
+        if (currentParent is null)
         {
-            var personIsActive = await _person.ExistsActiveAsync(command.PersonId.Value, cancellationToken);
-            if (!personIsActive)
-                return Result<Guid>.Failure("El confirmando/ahijado no existe o está inactivo");
-            _parentPerson.Add(new Domain.Parent.ParentPerson(
-                parent.ID,
-                command.PersonId.Value,
-                command.IsParent
-            ));
+            var resultCreate = await CreateAsync(command, normalizedName, cancellationToken);
+            if (!resultCreate.IsSuccess)
+                return Result<Guid>.Failure(resultCreate.Error!);
+            id = resultCreate.Value;
         }
-
+        else
+        {
+            var resultAssign = await AssignAsync(currentParent.ID, command.PersonId, command.IsParent, cancellationToken);
+            if (!resultAssign.IsSuccess)
+                return Result<Guid>.Failure(resultAssign.Error!);
+            id = currentParent.ID;
+        }
         await _save.SaveChangesAsync(cancellationToken);
-        _logs.LogInformation("Parent {parent} has been added by user {user}", parent.ID, command.UserId);
-        return Result<Guid>.Success(parent.ID);
+        return Result<Guid>.Success(id);
     }
 }
