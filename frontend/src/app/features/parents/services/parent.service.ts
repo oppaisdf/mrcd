c3fdbcb9749a87a06cd081f065017ccacbf46390
Josh <tarduvias@gmail.com>
2026-02-16 15:27:22 +0000

    feat(parents): list parent function in service

--- frontend/src/app/features/parents/services/parent.service.ts
+++ frontend/src/app/features/parents/services/parent.service.ts
@@ -1,6 +1,8 @@
 import { inject, Injectable } from '@angular/core';
 import { ApiService } from '../../../core/api/api.service';
 import { CreateParentRequest } from '../requests/create-parent.request';
+import { PagedResult } from '../../../core/api/api.types';
+import { AssignedParentResponse } from '../responses/assigned-parent.response';
 
 @Injectable()
 export class ParentService {
@@ -10,5 +12,16 @@ export class ParentService {
     request: CreateParentRequest
   ) {
     return this._api.postAsync<CreateParentRequest, string>('/parent', request);
+  }
+
+  public toListAsync(
+    page: number,
+    parentName: string | null
+  ) {
+    const params: Record<string, any> = {
+      page: page,
+      parentName: parentName
+    };
+    return this._api.getAsync<PagedResult<AssignedParentResponse>>('/parent', params);
   }
 }
