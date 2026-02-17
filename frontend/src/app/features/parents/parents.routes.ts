26135001ec367f9a9eb452ca12947b8eb8572654
Josh <tarduvias@gmail.com>
2026-02-17 14:22:10 +0000

    feat(parents): details parent route

--- frontend/src/app/features/parents/parents.routes.ts
+++ frontend/src/app/features/parents/parents.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from "@angular/router";
 import { ParentService } from "./services/parent.service";
+import { guidParamGuard } from "../../core/guards/guid-param-guard";
 
 export const PARENTS_ROUTES: Routes = [
     {
@@ -18,5 +19,12 @@ export const PARENTS_ROUTES: Routes = [
             ParentService
         ],
         loadComponent: () => import('./create/create-parents.page').then(p => p.CreateParentsPage)
+    }, {
+        path: ':id',
+        title: 'Detalles del padre/padrino',
+        canMatch: [guidParamGuard('id')],
+        data: { vtIndex: 9003 },
+        providers: [ParentService],
+        loadComponent: () => import('./details/parents-details.page').then(p => p.ParentsDetailsPage)
     }
 ];
