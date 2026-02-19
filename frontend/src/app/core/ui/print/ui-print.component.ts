4811b080db592f70c9e5f1a44464f631e14b4821
Josh <tarduvias@gmail.com>
2026-02-19 16:36:11 +0000

    feat(ui): orientation page in print component

--- frontend/src/app/core/ui/print/ui-print.component.ts
+++ frontend/src/app/core/ui/print/ui-print.component.ts
@@ -1,16 +1,37 @@
-import { Component, HostBinding, input } from '@angular/core';
+import { Component, HostBinding, inject, input } from '@angular/core';
 import { Paper } from './paper';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { UiSelectComponent } from '../select/ui-select.component';
+import { SelectItem } from '../select/SelectItem';
 
 @Component({
   selector: 'ui-print',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    UiSelectComponent
+  ],
   templateUrl: './ui-print.component.html',
   styleUrl: './ui-print.component.scss',
+  host: {
+    '[attr.data-paper]': 'paper()',
+    '[attr.data-orientation]': 'orientation'
+  }
 })
 export class UiPrintComponent {
+  private readonly _form = inject(FormBuilder);
+  readonly form = this._form.nonNullable.group({
+    orientation: ['portrait']
+  });
   @HostBinding('attr.data-print-root') printRoot = 'true';
   paper = input.required<Paper>();
   title = input<string>();
+  readonly orientations: Array<SelectItem<string>> = [{
+    label: 'Vertical',
+    value: 'portrait'
+  }, {
+    label: 'Horizontal',
+    value: 'landscape'
+  }];
 
   constructor() {
     window.addEventListener('afterprint', () => {
@@ -22,4 +43,6 @@ export class UiPrintComponent {
     document.documentElement.classList.add('print-mode');
     requestAnimationFrame(() => window.print());
   }
+
+  get orientation() { return this.form.controls.orientation.value; }
 }
