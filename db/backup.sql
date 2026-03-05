27079ddf7abe7530c8932d3d3ca0cdc8ef611b89
Misha <tarduvias@gmail.com>
2026-03-05 08:58:15 -0600

    add(db): logs table in backup

--- db/backup.sql
+++ db/backup.sql
@@ -235,6 +235,26 @@ INSERT INTO `document` VALUES
 UNLOCK TABLES;
 
 --
+-- Table structure for table `logs`
+--
+
+DROP TABLE IF EXISTS `logs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `logs` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `Timestamp` varchar(100) DEFAULT NULL,
+  `Level` varchar(15) DEFAULT NULL,
+  `Template` text,
+  `Message` text,
+  `Exception` text,
+  `Properties` text,
+  `_ts` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=135 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Table structure for table `parent`
 --
 
