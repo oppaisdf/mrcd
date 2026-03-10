49a723d86f6f6eb6667fb56aa77e159d1ff35670
Misha <tarduvias@gmail.com>
2026-03-10 10:11:20 -0600

    fix(app): change user ID by username in log handler

--- backend/MRCD.Application/Logs/GetLogs/GetLogsHandler.cs
+++ backend/MRCD.Application/Logs/GetLogs/GetLogsHandler.cs
@@ -2,23 +2,33 @@ using MRCD.Application.Abstracts.Handlers;
 using MRCD.Application.Common;
 using MRCD.Application.Logs.Contracts;
 using MRCD.Application.Logs.DTOs;
+using MRCD.Application.User.Contracts;
 using MRCD.Domain.Common;
 
 namespace MRCD.Application.Logs.GetLogs;
 
 internal sealed class GetLogsHandler(
-    ILogRepository repo
+    ILogRepository repo,
+    IUserRepository user
 ) : IQueryHandler<Pagination<LogDTO>, GetLogsQuery>
 {
     private readonly ILogRepository _repo = repo;
+    private readonly IUserRepository _user = user;
 
-    public Task<Result<Pagination<LogDTO>>> HandleAsync(
+    public async Task<Result<Pagination<LogDTO>>> HandleAsync(
         GetLogsQuery query,
         CancellationToken cancellationToken
-    ) => _repo
-        .ToListAsync(
+    ) {
+        var rawLogs = await _repo.ToListAsync(
             query.Size > 100 ? (ushort)100 : query.Size,
             query.Page,
             cancellationToken
-        ).ContinueWith(r => Result<Pagination<LogDTO>>.Success(r.Result), cancellationToken);
+        );
+        var usersDir = (await _user.ToListAsync(cancellationToken))
+            .ToDictionary(k => k.ID.ToString(), k => k.Username);
+        var logs = rawLogs with {
+            Items = rawLogs.Items.Select(l => l with {Username = usersDir.TryGetValue(l.Username, out string username) ? username : "unknown"})
+        };
+        return Result<Pagination<LogDTO>>.Success(logs);
+    }
 }
