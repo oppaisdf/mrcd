371b5b30e6f21c7bfd1ff03cd06989539bb870c5
Misha <tarduvias@gmail.com>
2026-06-23 15:54:04 -0600

    feat(app): remove empty dates getting attendanes

--- backend/MRCD.Application/Attendance/GetAttendance/GetAttendanceHandler.cs
+++ backend/MRCD.Application/Attendance/GetAttendance/GetAttendanceHandler.cs
@@ -25,35 +25,49 @@ internal sealed class GetAttendaceHandler(
         var normalizedPersonName = string.IsNullOrWhiteSpace(query.PersonName)
             ? null
             : _service.NormalizeString(query.PersonName);
+
         var attendances = await _repo.ToListAsync(
             query.Date,
             query.FilteredOnlyByYear,
             cancellationToken
         );
-        var dates = attendances
-            .Select(ra => ra.Date)
-            .Distinct()
-            .ToList();
+
         var rawPeople = await _person.OnlyActiveToListAsync(cancellationToken);
         var people = rawPeople
             .Where(p =>
                 (query.IsSunday is null || query.IsSunday == p.IsSunday)
                 && (query.IsMasculine is null || query.IsMasculine == p.IsMasculine)
                 && (normalizedPersonName is null || p.NormalizedName.Contains(normalizedPersonName))
-            );
+            )
+            .ToList();
+
+        var peopleIds = people
+            .Select(p => p.ID)
+            .ToHashSet();
+
+        var dates = attendances
+            .Where(a => peopleIds.Contains(a.PersonId))
+            .Select(a => a.Date)
+            .Distinct()
+            .OrderBy(d => d)
+            .ToList();
+
         var attendanceDir = attendances
             .GroupBy(a => (a.PersonId, a.Date))
             .ToDictionary(g => g.Key, g => g.Single());
-        return Result<IEnumerable<AttendanceDTO>>.Success(people.Select(p => new AttendanceDTO(
-            p.Name,
-            dates.Select(d => new HasAttendanceDTO(
-                d,
-                attendanceDir.TryGetValue((p.ID, d), out var a)
-                    ? a.IsAttendance
-                        ? AttendanceType.Attended
-                        : AttendanceType.Excused
+
+        return Result<IEnumerable<AttendanceDTO>>.Success(
+            people.Select(p => new AttendanceDTO(
+                p.Name,
+                dates.Select(d => new HasAttendanceDTO(
+                    d,
+                    attendanceDir.TryGetValue((p.ID, d), out var a)
+                        ? a.IsAttendance
+                            ? AttendanceType.Attended
+                            : AttendanceType.Excused
                         : AttendanceType.Absent
+                ))
             ))
-        )));
+        );
     }
 }
