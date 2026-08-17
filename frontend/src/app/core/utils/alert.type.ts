f264919f71528fd000a3365d08ac1facf86756ac
Misha <tarduvias@gmail.com>
2026-08-17 20:45:01 +0000

    add(core): alert type

---
+++ frontend/src/app/core/utils/alert.type.ts
@@ -0,0 +1,6 @@
+export enum AlertType {
+    LONELY_PARENTS = 'lonely-parents',
+    PENDING_CHARGES = 'pending-charges',
+    PENDING_DOCUMENTS = 'pending-documents',
+    PENDING_GODPARENTS = 'pending-godparents'
+}
