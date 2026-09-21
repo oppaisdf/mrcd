feea668572eab4407db4fedef38afdbbb49feeec
Misha <tarduvias@gmail.com>
2026-09-21 19:20:23 +0000

    feat(accounting): list accounting movements function in service

--- frontend/src/app/features/accounting-movement/services/accounting-movement.service.ts
+++ frontend/src/app/features/accounting-movement/services/accounting-movement.service.ts
@@ -1,6 +1,7 @@
 import { inject, Injectable } from '@angular/core';
 import { ApiService } from '../../../core/api/api.service';
 import { CreateAccountingMovementRequest } from '../requests/create-accounting-movement.request';
+import { AccountingMovementResponse } from '../responses/accounting-movement.response';
 
 @Injectable()
 export class AccountingMovementService {
@@ -10,5 +11,16 @@ export class AccountingMovementService {
     request: CreateAccountingMovementRequest
   ){
     return this._api.postAsync('/accountingmovement', request);
+  }
+
+  toListAsync(
+    filterOnlyByYear: boolean,
+    date: Date
+  ){
+    const params: Record<string, any> = {
+      filterOnlyByYear: filterOnlyByYear,
+      date: new Date(date).toISOString().split('T')[0]
+    }
+    return this._api.getAsync<Array<AccountingMovementResponse>>(`/accountingmovement`, params);
   }
 }
