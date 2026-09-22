fd8dbbb674dd12729c5aa2acf3267ebbbea2b68f
Misha <tarduvias@gmail.com>
2026-09-22 16:16:20 +0000

    feat(dom): validate name creating permision

--- backend/MRCD.Domain/Role/Permission.cs
+++ backend/MRCD.Domain/Role/Permission.cs
@@ -1,10 +1,15 @@
 using MRCD.Domain.Common;
+using System.Text.RegularExpressions;
 
 namespace MRCD.Domain.Role;
 
-public sealed class Permission : BaseEntity
+public sealed partial class Permission : BaseEntity
 {
     private Permission() { }
+
+    [GeneratedRegex("^[A-Za-z]+\\.[A-Za-z]+$")]
+    private static partial Regex PermissionName();
+    
     public static Result<Permission> Create(
         string name
     )
@@ -13,6 +18,8 @@ public sealed class Permission : BaseEntity
             return Result<Permission>.Failure("El nombre del permiso no puede estar vacío");
         if (name.Trim().Length > 30)
             return Result<Permission>.Failure("El nombre del permiso no puede exceder los 30 caracteres");
+        if (!PermissionName().IsMatch(name.Trim()))
+            return Result<Permission>.Failure("El nombre del permiso es inválido");
         return Result<Permission>.Success(new()
         {
             ID = Guid.NewGuid(),
