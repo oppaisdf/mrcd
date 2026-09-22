defcfdfce5fcf9743636aa9aa4ea3880f0d879e5
Misha <tarduvias@gmail.com>
2026-09-22 19:46:51 +0000

    refac(app): use function creating role in handler

--- backend/MRCD.Application/Role/Add/AddRoleHandler.cs
+++ backend/MRCD.Application/Role/Add/AddRoleHandler.cs
@@ -2,13 +2,11 @@ using MRCD.Application.Logs;
 using MRCD.Application.Abstracts;
 using MRCD.Application.Abstracts.Handlers;
 using MRCD.Application.Role.Contracts;
-using MRCD.Application.Services.Common;
 using MRCD.Domain.Common;
 
 namespace MRCD.Application.Role.Add;
 
 internal sealed class AddRoleHandler(
-    ICommonService service,
     IRoleRepository repo,
     IPersistenceContext save,
     AuditLog<AddRoleHandler> logs
@@ -23,18 +21,15 @@ internal sealed class AddRoleHandler(
         CancellationToken cancellationToken
     )
     {
-        if (string.IsNullOrWhiteSpace(command.RoleName))
-            return Result<Guid>.Failure("El nombre del rol no puede ser nulo");
-        if (!_service.HasOnlyLetters(command.RoleName))
-            return Result<Guid>.Failure("El nombre del rol solo puede contener letras");
+        var role = Domain.Role.Role.Create(command.RoleName);
+        if (!role.IsSuccess) return Result<Guid>.Failure(role.Error!);
         var alreadExists = await _repo.AlreadyExistsAsync(command.RoleName.Trim(), cancellationToken);
         if (alreadExists)
             return Result<Guid>.Failure("El nombre del rol ya está en uso");
 
-        var role = Domain.Role.Role.Create(command.RoleName.Trim());
-        if (!role.IsSuccess && role.Value is null) return Result<Guid>.Failure(role.Error!);
         _repo.Add(role.Value!);
         await _save.SaveChangesAsync(cancellationToken);
         _logs.Write(command.UserId, "Role {role} with ID {id} has been created.", command.RoleName, role.Value!.ID);
+        return Result<Guid>.Success(role.Value!.ID);
     }
 }
