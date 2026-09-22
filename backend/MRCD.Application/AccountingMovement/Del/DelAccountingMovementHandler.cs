ea3e4171fd7fc0043b21bbd5a76e93c522ae20e7
Misha <tarduvias@gmail.com>
2026-09-22 17:13:13 +0000

    refac(app): use audit logger deleting accounting movement handler

--- backend/MRCD.Application/AccountingMovement/Del/DelAccountingMovementHandler.cs
+++ backend/MRCD.Application/AccountingMovement/Del/DelAccountingMovementHandler.cs
@@ -1,4 +1,4 @@
-using Microsoft.Extensions.Logging;
+using MRCD.Application.Logs;
 using MRCD.Application.Abstracts.Handlers;
 using MRCD.Application.AccountingMovement.Contracts;
 using MRCD.Domain.Common;
@@ -7,11 +7,11 @@ namespace MRCD.Application.AccountingMovement.Del;
 
 internal sealed class DelAccountingMovementHandler(
     IAccountingMovementRepository repo,
-    ILogger<DelAccountingMovementHandler> logs
+    AuditLog<DelAccountingMovementHandler> logs
 ) : ICommandHandler<DelAccountingMovementCommand>
 {
     private readonly IAccountingMovementRepository _repo = repo;
-    private readonly ILogger<DelAccountingMovementHandler> _logs = logs;
+    private readonly AuditLog<DelAccountingMovementHandler> _logs = logs;
 
     public async Task<Result> HandleAsync(
         DelAccountingMovementCommand command,
@@ -22,13 +22,7 @@ internal sealed class DelAccountingMovementHandler(
         if (!exists)
             return Result.Failure("El movimiento contable no existe");
         await _repo.DeleteAsync(command.MovementId, cancellationToken);
-        using (_logs.BeginScope(new Dictionary<string, object>
-        {
-            ["UserId"] = command.UserId
-        }))
-        {
-            _logs.LogInformation("Accounting movement {moement} has been deleted.", command.MovementId);
-        }
+        _logs.Write(command.UserId, "Accounting movement {moement} has been deleted.", command.MovementId);
         return Result.Success();
     }
 }
