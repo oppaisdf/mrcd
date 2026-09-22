3616b6d3fd87a52c6f1c56d062792949fcf5d88b
Misha <tarduvias@gmail.com>
2026-09-22 17:23:52 +0000

    refac(app): use creating function from dom adding permission handler

--- backend/MRCD.Application/Permission/Add/AddPermissionHandler.cs
+++ backend/MRCD.Application/Permission/Add/AddPermissionHandler.cs
@@ -1,34 +1,28 @@
 using MRCD.Application.Abstracts;
 using MRCD.Application.Abstracts.Handlers;
 using MRCD.Application.Permission.Contracts;
-using MRCD.Application.Services.Common;
 using MRCD.Domain.Common;
 
 namespace MRCD.Application.Permission.Add;
 
 internal sealed class AddPermissionHandler(
-    ICommonService service,
     IPermissionRepository repo,
     IPersistenceContext save
 ) : ICommandHandler<AddPermissionCommand, Guid>
 {
     private readonly IPermissionRepository _repo = repo;
     private readonly IPersistenceContext _save = save;
-    private readonly ICommonService _service = service;
 
     public async Task<Result<Guid>> HandleAsync(
         AddPermissionCommand command,
         CancellationToken cancellationToken
     )
     {
-        if (!_service.IsValidPermission(command.PermissionName))
-            return Result<Guid>.Failure("El nombre del permiso es inválido");
+        var permission = Domain.Role.Permission.Create(command.PermissionName);
+        if (!permission.IsSuccess) return Result<Guid>.Failure(permission.Error!);
         var alreadyExists = await _repo.AlreadyExistsAsync(command.PermissionName.Trim(), cancellationToken);
         if (alreadyExists)
             return Result<Guid>.Failure("El nombre del permiso ya existe");
-        var permission = Domain.Role.Permission.Create(command.PermissionName.Trim());
-        if (!permission.IsSuccess)
-            return Result<Guid>.Failure(permission.Error!);
         _repo.Add(permission.Value!);
         await _save.SaveChangesAsync(cancellationToken);
         return Result<Guid>.Success(permission.Value!.ID);
