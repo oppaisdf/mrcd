93e4b55ed2cc363bdbf780cd85544a0cfa6da02b
Misha <tarduvias@gmail.com>
2026-09-22 19:42:06 +0000

    refac(app): use audit log deleting activity handler

--- backend/MRCD.Application/Planner/Del/DelActivityHandler.cs
+++ backend/MRCD.Application/Planner/Del/DelActivityHandler.cs
@@ -1,4 +1,4 @@
-using Microsoft.Extensions.Logging;
+using MRCD.Application.Logs;
 using MRCD.Application.Abstracts.Handlers;
 using MRCD.Application.Planner.Contracts;
 using MRCD.Domain.Common;
@@ -7,11 +7,11 @@ namespace MRCD.Application.Planner.Del;
 
 internal sealed class DelActivityHandler(
     IActivityRepository repo,
-    ILogger<DelActivityHandler> logs
+    AuditLog<DelActivityHandler> logs
 ) : ICommandHandler<DelActivityCommand>
 {
     private readonly IActivityRepository _repo = repo;
-    private readonly ILogger<DelActivityHandler> _logs = logs;
+    private readonly AuditLog<DelActivityHandler> _logs = logs;
 
     public async Task<Result> HandleAsync(
         DelActivityCommand command,
@@ -22,13 +22,7 @@ internal sealed class DelActivityHandler(
         if (!exists)
             return Result.Failure("La actividad no existe");
         await _repo.DeleteAsync(command.ActivityId, cancellationToken);
-        using (_logs.BeginScope(new Dictionary<string, object>
-        {
-            ["UserId"] = command.UserId
-        }))
-        {
-            _logs.LogInformation("Activity {activity} has been deleted.", command.ActivityId);
-        }
+        _logs.Write(command.UserId, "Activity {activity} has been deleted.", command.ActivityId);
         return Result.Success();
     }
 }
