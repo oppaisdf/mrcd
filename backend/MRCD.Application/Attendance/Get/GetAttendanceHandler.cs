038dd438ff8fcb308a4999a726440bbcfc06671c
Misha <tarduvias@gmail.com>
2026-09-24 19:23:43 +0000

    refac(app): use normalizer getting attendnace

--- backend/MRCD.Application/Attendance/Get/GetAttendanceHandler.cs
+++ backend/MRCD.Application/Attendance/Get/GetAttendanceHandler.cs
@@ -2,21 +2,19 @@ using MRCD.Application.Abstracts.Handlers;
 using MRCD.Application.Attendance.Contracts;
 using MRCD.Application.Attendance.DTOs;
 using MRCD.Application.Person.Contracts;
+using MRCD.Application.Services;
 using MRCD.Application.Services.Attendance;
-using MRCD.Application.Services.Common;
 using MRCD.Domain.Common;
 
 namespace MRCD.Application.Attendance.Get;
 
 internal sealed class GetAttendaceHandler(
     IAttendanceRepository repo,
-    ICommonService cservice,
     IPersonRepository person,
     IAttendanceService service
 ) : IQueryHandler<IEnumerable<AttendanceDTO>, GetAttendanceQuery>
 {
     private readonly IAttendanceRepository _repo = repo;
-    private readonly ICommonService _cservice = cservice;
     private readonly IPersonRepository _person = person;
     private readonly IAttendanceService _service = service;
 
@@ -27,7 +25,7 @@ internal sealed class GetAttendaceHandler(
     {
         var normalizedPersonName = string.IsNullOrWhiteSpace(query.PersonName)
             ? null
-            : _cservice.NormalizeString(query.PersonName);
+            : StringNormalizer.NormalizeString(query.PersonName);
 
         var attendances = await _repo.ToListAsync(
             query.Date,
