f0f3992f2ecb72041e17bb9fc0301855fb322389
Misha <tarduvias@gmail.com>
2026-09-24 20:01:47 +0000

    refac(app): simplify del accounting movement handler

--- backend/MRCD.Application/AccountingMovement/Del/DelAccountingMovementHandler.cs
+++ backend/MRCD.Application/AccountingMovement/Del/DelAccountingMovementHandler.cs
@@ -10,19 +10,16 @@ internal sealed class DelAccountingMovementHandler(
     AuditLog<DelAccountingMovementHandler> logs
 ) : ICommandHandler<DelAccountingMovementCommand>
 {
-    private readonly IAccountingMovementRepository _repo = repo;
-    private readonly AuditLog<DelAccountingMovementHandler> _logs = logs;
-
     public async Task<Result> HandleAsync(
         DelAccountingMovementCommand command,
         CancellationToken cancellationToken
     )
     {
-        var exists = await _repo.ExistsIdAsync(command.MovementId, cancellationToken);
+        var exists = await repo.ExistsIdAsync(command.MovementId, cancellationToken);
         if (!exists)
             return Result.Failure("El movimiento contable no existe");
-        await _repo.DeleteAsync(command.MovementId, cancellationToken);
-        _logs.Write(command.UserId, "Accounting movement {moement} has been deleted.", command.MovementId);
+        await repo.DeleteAsync(command.MovementId, cancellationToken);
+        logs.Write(command.UserId, "Accounting movement {moement} has been deleted.", command.MovementId);
         return Result.Success();
     }
 }
