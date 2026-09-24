51436a8a86840137eaef85744fdce0407c1393a4
Misha <tarduvias@gmail.com>
2026-09-24 20:57:20 +0000

    refac(app): simplify login handler

--- backend/MRCD.Application/User/Login/UserLoginHandler.cs
+++ backend/MRCD.Application/User/Login/UserLoginHandler.cs
@@ -7,13 +7,10 @@ using MRCD.Domain.Common;
 namespace MRCD.Application.User.Login;
 
 internal sealed class UserLoginHandler(
-    IUserRepository user,
+    IUserRepository userRepo,
     IRoleRepository role
 ) : ICommandHandler<UserLoginCommand, LoginDTO>
 {
-    private readonly IUserRepository _user = user;
-    private readonly IRoleRepository _role = role;
-
     public async Task<Result<LoginDTO>> HandleAsync(
         UserLoginCommand command,
         CancellationToken cancellationToken
@@ -21,11 +18,11 @@ internal sealed class UserLoginHandler(
     {
         if (string.IsNullOrWhiteSpace(command.Username))
             return Result<LoginDTO>.Failure("El usuario es requerido");
-        var user = await _user.GetByUsernameAsync(command.Username.Trim(), cancellationToken);
+        var user = await userRepo.GetByUsernameAsync(command.Username.Trim(), cancellationToken);
         if (user is null || !user.IsActive || !user.Password.Equals(command.Password))
             return Result<LoginDTO>.Failure("Credenciales inválidas");
 
-        var roles = await _role.ByUserIdToListAsync(user.ID, cancellationToken);
+        var roles = await role.ByUserIdToListAsync(user.ID, cancellationToken);
         return Result<LoginDTO>.Success(new(
             user.ID,
             roles.Select(r => r.Name)
