22508ececae9c147ee4a2d1f3a72e93984c9ef2b
Misha <tarduvias@gmail.com>
2026-09-24 21:25:26 +0000

    refac(infra): simplify user repo

--- backend/MRCD.Infrastructure/Repositories/UserRepository.cs
+++ backend/MRCD.Infrastructure/Repositories/UserRepository.cs
@@ -8,32 +8,30 @@ internal sealed class UserRepository(
     Persistence.AppContext app
 ) : IUserRepository
 {
-    private readonly Persistence.AppContext _app = app;
-
     public void Add(
         User user
-    ) => _app
+    ) => app
         .Users
         .Add(user);
 
     public Task<User?> GetByIdAsync(
         Guid id,
         CancellationToken cancellationToken
-    ) => _app
+    ) => app
         .Users
         .SingleOrDefaultAsync(u => u.ID == id, cancellationToken);
 
     public Task<User?> GetByUsernameAsync(
         string username,
         CancellationToken cancellationToken
-    ) => _app
+    ) => app
         .Users
         .SingleOrDefaultAsync(u => u.Username == username, cancellationToken);
 
     public Task<bool> IsActiveAsync(
         Guid userId,
         CancellationToken cancellationToken
-    ) => _app
+    ) => app
         .Users
         .AnyAsync(u =>
             u.ID == userId
@@ -43,7 +41,7 @@ internal sealed class UserRepository(
 
     public Task<List<User>> ToListAsync(
         CancellationToken cancellationToken
-    ) => _app
+    ) => app
         .Users
         .AsNoTracking()
         .ToListAsync(cancellationToken);
