09264369fddc59a78d8381f605406c4b28d4f638
Misha <tarduvias@gmail.com>
2026-09-24 20:20:03 +0000

    refac(app): simplify del permission handler

--- backend/MRCD.Application/Permission/Del/DelPermissionHandler.cs
+++ backend/MRCD.Application/Permission/Del/DelPermissionHandler.cs
@@ -10,18 +10,15 @@ internal sealed class DelPermissionHandler(
     AuditLog<DelPermissionHandler> logs
 ) : ICommandHandler<DelPermissionCommand>
 {
-    private readonly IPermissionRepository _repo = repo;
-    private readonly AuditLog<DelPermissionHandler> _logs = logs;
-
     public async Task<Result> HandleAsync(
         DelPermissionCommand command,
         CancellationToken cancellationToken
     )
     {
-        var exists = await _repo.IdExistsAsync(command.PermissionId, cancellationToken);
+        var exists = await repo.IdExistsAsync(command.PermissionId, cancellationToken);
         if (!exists) return Result.Failure("El permiso no existe");
-        await _repo.DeleteAsync(command.PermissionId, cancellationToken);
-        _logs.Write(command.UserId, "Permission {permission} has been deleted.", command.PermissionId);
+        await repo.DeleteAsync(command.PermissionId, cancellationToken);
+        logs.Write(command.UserId, "Permission {permission} has been deleted.", command.PermissionId);
         return Result.Success();
     }
 }
