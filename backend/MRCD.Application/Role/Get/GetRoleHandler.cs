21b46a7f1940a81b5fdd5d841ef16d7303d53b87
Misha <tarduvias@gmail.com>
2026-09-24 20:51:15 +0000

    refac(app): simplify get role handler

--- backend/MRCD.Application/Role/Get/GetRoleHandler.cs
+++ backend/MRCD.Application/Role/Get/GetRoleHandler.cs
@@ -8,11 +8,9 @@ internal sealed class GetRoleHandler(
     IRoleRepository repo
 ) : IQueryHandler<List<Domain.Role.Role>>
 {
-    private readonly IRoleRepository _repo = repo;
-
     public Task<Result<List<Domain.Role.Role>>> HandleAsync(
         CancellationToken cancellationToken
-    ) => _repo
+    ) => repo
         .ToListAsync(cancellationToken)
         .ContinueWith(r => Result<List<Domain.Role.Role>>.Success(r.Result), cancellationToken);
 }
