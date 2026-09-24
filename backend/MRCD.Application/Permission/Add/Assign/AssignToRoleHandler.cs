565784337ccaf63a93841532e261f4a2e922bbaf
Misha <tarduvias@gmail.com>
2026-09-24 20:18:47 +0000

    refac(app): simplify assign to role handler

--- backend/MRCD.Application/Permission/Add/Assign/AssignToRoleHandler.cs
+++ backend/MRCD.Application/Permission/Add/Assign/AssignToRoleHandler.cs
@@ -18,31 +18,24 @@ internal sealed class AssignToRoleHandler(
     PermissionCacheInvalidator cache
 ) : ICommandHandler<AssignToRoleCommand>
 {
-    private readonly IPermissionRepository _permission = permission;
-    private readonly IRoleRepository _role = role;
-    private readonly IRolePermissionRepository _rolePermission = rolePermission;
-    private readonly AuditLog<AssignToRoleHandler> _logs = logs;
-    private readonly IPersistenceContext _save = save;
-    private readonly PermissionCacheInvalidator _cache = cache;
-
     public async Task<Result> HandleAsync(
         AssignToRoleCommand command,
         CancellationToken cancellationToken
     )
     {
         var exists =
-            await _role.IdExistsAsync(command.RoleId, cancellationToken)
-            && await _permission.IdExistsAsync(command.PermissionId, cancellationToken);
+            await role.IdExistsAsync(command.RoleId, cancellationToken)
+            && await permission.IdExistsAsync(command.PermissionId, cancellationToken);
         if (!exists)
             return Result.Failure("El rol o el permiso no existe :c");
-        var alreadyExists = await _rolePermission.ExistsAsync(command.RoleId, command.PermissionId, cancellationToken);
+        var alreadyExists = await rolePermission.ExistsAsync(command.RoleId, command.PermissionId, cancellationToken);
         if (alreadyExists)
             return Result.Failure("El permiso no se puede asignar al rol porque ya ha sido asignado :0");
-        var rolePermission = new RolePermission(command.RoleId, command.PermissionId);
-        _rolePermission.Add(rolePermission);
-        await _save.SaveChangesAsync(cancellationToken);
-        await _cache.InvalidateActiveUsersAsync(cancellationToken);
-        _logs.Write(command.UserId, "Permission {permission} has been assigned to role {role}", command.PermissionId, command.RoleId);
+        var _rolePermission = new RolePermission(command.RoleId, command.PermissionId);
+        rolePermission.Add(_rolePermission);
+        await save.SaveChangesAsync(cancellationToken);
+        await cache.InvalidateActiveUsersAsync(cancellationToken);
+        logs.Write(command.UserId, "Permission {permission} has been assigned to role {role}", command.PermissionId, command.RoleId);
         return Result.Success();
     }
 }
