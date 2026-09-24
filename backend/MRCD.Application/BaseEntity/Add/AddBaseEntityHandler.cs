49638741a8b8bdea3c4c11ca4e5f134c4740aefa
Misha <tarduvias@gmail.com>
2026-09-24 20:08:25 +0000

    refac(app): simplify add base entity handler

--- backend/MRCD.Application/BaseEntity/Add/AddBaseEntityHandler.cs
+++ backend/MRCD.Application/BaseEntity/Add/AddBaseEntityHandler.cs
@@ -15,22 +15,17 @@ internal sealed class AddBaseEntityHandler<TEntity>(
 ) : ICommandHandler<AddBaseEntityCommand, Guid, TEntity>
     where TEntity : Domain.Common.BaseEntity
 {
-    private readonly IBaseEntityRepository<TEntity> _repo = repo;
-    private readonly NamedEntityPreparation<TEntity> _preparation = preparation;
-    private readonly IPersistenceContext _save = save;
-    private readonly AuditLog<AddBaseEntityHandler<TEntity>> _logs = logs;
-
     public async Task<Result<Guid>> HandleAsync(
         AddBaseEntityCommand command,
         CancellationToken cancellationToken
     )
     {
-        var newRecord = await _preparation.PrepareAsync(command.Name, cancellationToken);
+        var newRecord = await preparation.PrepareAsync(command.Name, cancellationToken);
         if (!newRecord.IsSuccess)
             return Result<Guid>.Failure(newRecord.Error!);
-        _repo.Add(newRecord.Value!);
-        await _save.SaveChangesAsync(cancellationToken);
-        _logs.Write(command.UserId, "Record {record} with ID {id} has been created.", command.Name, newRecord.Value!.ID);
+        repo.Add(newRecord.Value!);
+        await save.SaveChangesAsync(cancellationToken);
+        logs.Write(command.UserId, "Record {record} with ID {id} has been created.", command.Name, newRecord.Value!.ID);
         return Result<Guid>.Success(newRecord.Value!.ID);
     }
 }
