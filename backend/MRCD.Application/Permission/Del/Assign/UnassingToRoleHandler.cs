04dadead4d4c115f8e032aabccf17f562eaa3e80
Misha <tarduvias@gmail.com>
2026-09-24 20:20:43 +0000

    refac(app): simplify unassing to role handler

--- backend/MRCD.Application/Permission/Del/Assign/UnassingToRoleHandler.cs
+++ backend/MRCD.Application/Permission/Del/Assign/UnassingToRoleHandler.cs
@@ -12,29 +12,25 @@ internal sealed class UnassignToRoleHandler(
     PermissionCacheInvalidator cache
 ) : ICommandHandler<UnassignToRoleCommand>
 {
-    private readonly IRolePermissionRepository _rolePermission = rolePermission;
-    private readonly AuditLog<UnassignToRoleHandler> _logs = logs;
-    private readonly PermissionCacheInvalidator _cache = cache;
-
     public async Task<Result> HandleAsync(
         UnassignToRoleCommand command,
         CancellationToken cancellationToken
     )
     {
-        var exists = await _rolePermission.ExistsAsync(
+        var exists = await rolePermission.ExistsAsync(
             command.RoleId,
             command.PermissionId,
             cancellationToken
         );
         if (!exists)
             return Result.Failure("El permiso asignado al rol no existe");
-        await _rolePermission.DeleteAsync(
+        await rolePermission.DeleteAsync(
             command.RoleId,
             command.PermissionId,
             cancellationToken
         );
-        _logs.Write(command.UserId, "The permission {permission} has been removed from role {role}", command.PermissionId, command.RoleId);
-        await _cache.InvalidateActiveUsersAsync(cancellationToken);
+        logs.Write(command.UserId, "The permission {permission} has been removed from role {role}", command.PermissionId, command.RoleId);
+        await cache.InvalidateActiveUsersAsync(cancellationToken);
         return Result.Success();
     }
 }
