269afdf1087c90bdcb198d5ebb74f10a78db4a22
Misha <tarduvias@gmail.com>
2026-09-24 20:55:08 +0000

    refac(app): simplify assign role handler

--- backend/MRCD.Application/User/Add/Assign/AssignRoleHandler.cs
+++ backend/MRCD.Application/User/Add/Assign/AssignRoleHandler.cs
@@ -9,47 +9,41 @@ using MRCD.Domain.User;
 namespace MRCD.Application.User.Add.Assign;
 
 internal sealed class AssignRoleHandler(
-    IUserRepository user,
-    IRoleRepository role,
+    IUserRepository userRepo,
+    IRoleRepository roleRepo,
     IUserRoleRepository userRole,
     IPersistenceContext save,
     IPermissionCache cache
 ) : ICommandHandler<AssignRoleCommand>
 {
-    private readonly IUserRepository _user = user;
-    private readonly IRoleRepository _role = role;
-    private readonly IUserRoleRepository _userRole = userRole;
-    private readonly IPersistenceContext _save = save;
-    private readonly IPermissionCache _cache = cache;
-
     public async Task<Result> HandleAsync(
         AssignRoleCommand command,
         CancellationToken cancellationToken
     )
     {
-        var user = await _user.GetByIdAsync(command.UserId, cancellationToken);
+        var user = await userRepo.GetByIdAsync(command.UserId, cancellationToken);
         if (user is null || !user.IsActive)
             return Result.Failure("El usuario no existe o está inactivo");
-        var role = await _role.GetByIdAsync(command.RoleId, cancellationToken);
+        var role = await roleRepo.GetByIdAsync(command.RoleId, cancellationToken);
         if (role is null || role.Name.Equals("sys"))
             return Result.Failure("El rol no existe");
-        var userRoles = (await _role.ByUserIdToListAsync(command.UserId, cancellationToken))
+        var userRoles = (await roleRepo.ByUserIdToListAsync(command.UserId, cancellationToken))
             .Select(r => r.ID);
         if (command.IsAssignment && userRoles.Contains(command.RoleId))
             return Result.Failure("El rol ya ha sido asignado");
         if (command.IsAssignment)
         {
-            _userRole.Add(new UserRole(
+            userRole.Add(new UserRole(
                 command.RoleId,
                 command.UserId
             ));
-            await _save.SaveChangesAsync(cancellationToken);
+            await save.SaveChangesAsync(cancellationToken);
         }
         if (!command.IsAssignment && !userRoles.Contains(command.RoleId))
             return Result.Failure("El rol no ha sido asignado");
         if (!command.IsAssignment)
-            await _userRole.DeleteAsync(command.UserId, command.RoleId, cancellationToken);
-        await _cache.InvalidateAsync(command.UserId, cancellationToken);
+            await userRole.DeleteAsync(command.UserId, command.RoleId, cancellationToken);
+        await cache.InvalidateAsync(command.UserId, cancellationToken);
         return Result.Success();
     }
 }
